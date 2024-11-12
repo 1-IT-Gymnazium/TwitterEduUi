082c2349036f9d91ce661ef2b4157e9f6fed1ec3
Ladislav Kožej <amadeo.decay@gmail.com>
2024-11-12 20:17:16 +0100

    clean up

--- src/app/resolvers/post-detail-resolver.ts
+++ src/app/resolvers/post-detail-resolver.ts
@@ -4,21 +4,25 @@ import { PostService } from '../services/post.service';
 import { inject } from '@angular/core';
 import { catchError, of } from 'rxjs';
 
-export const postDetailResolver: ResolveFn<undefined | PostDetail> = (route, state) => {
+export const postDetailResolver: ResolveFn<undefined | PostDetail> = (
+  route,
+  state
+) => {
   const postId = route.paramMap.get('postId');
   const router = inject(Router);
   const postService = inject(PostService);
 
-  if(!postId) {
+  if (!postId) {
     // Message // Toaster
     console.error('Post Id was not found');
     return of(undefined);
   }
 
-
-  return postService.getPost(postId).pipe(catchError(async (err) => {
-    console.log(err);
-    await router.navigate(['']);
-    throw of(err)
-  }));
+  return postService.getPost(postId).pipe(
+    catchError(async (err) => {
+      console.log(err);
+      await router.navigate(['']);
+      throw of(err);
+    })
+  );
 };
